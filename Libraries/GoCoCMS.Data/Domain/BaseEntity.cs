97050fdb901cd16b97c3dc8815c364858d2ae973
Khang Nguyen <nlkhang.it@gmail.com>
2018-10-13 21:32:15 +0700

    - add category, base entity domain model

---
+++ Libraries/GoCoCMS.Data/Domain/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace GoCoCMS.Data.Domain
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Libraries/GoCoCMS.Data/Domain/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace GoCoCMS.Data.Domain
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public int ParentCategoryId { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
