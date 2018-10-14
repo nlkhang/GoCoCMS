1060b1674c1a9717453fdddec68e8f4f5a3a0f41
Khang Nguyen <nlkhang.it@gmail.com>
2018-10-14 20:31:19 +0700

    add admin layout

--- Presentation/GoCoCMS.Web/Startup.cs
+++ Presentation/GoCoCMS.Web/Startup.cs
@@ -55,9 +55,16 @@ namespace GoCoCMS.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes => routes.MapRoute(
-                name: "default",
-                template: "{controller=Home}/{action=Index}/{id?}"));
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
